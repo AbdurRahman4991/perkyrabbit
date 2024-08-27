298ce7ec360815f637edc4044691f7e1030b9cfc
AbdurRahman4991 <engrabdurrahman4991@gmail.com>
2024-08-27 15:12:09 +0600

    emply fatching

--- Frontend/src/component/Emplyee.jsx
+++ Frontend/src/component/Emplyee.jsx
@@ -1,8 +1,9 @@
-
 import React, { useState } from 'react';
-import { Divider, Radio, Table } from 'antd';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Table } from 'antd';
+import { Container, Row, Col } from 'react-bootstrap';
 import { useGetEmplyeeQuery } from '../../redux/serivce/productlslice';
+
+
 const columns = [
     {
         title: 'Name',
@@ -15,83 +16,57 @@ const columns = [
     },
     {
         title: 'Phone',
-        dataIndex:'phone',
-
+        dataIndex: 'phone',
     },
-
     {
         title: 'Address',
         dataIndex: 'address',
-        
     },
     {
-        title:'Achievement',
-        dataIndex:'achievement',
+        title: 'Achievement',
+        dataIndex: 'achievement',
+        render: (text) => text || 'N/A', // Assuming some employees might not have achievements
     },
     {
-        title:'Action',
-        dataIndex:'action',
-        render: (text) => 
-        {
-            return(
-                <div>
-                    <button className='btn btn-success m-2'>Edit</button>
-                    <button className='btn btn-danger'> Delete</button>
-                </div>
-            )
-        }
-
+        title: 'Action',
+        dataIndex: 'action',
+        render: (_, record) => (
+            <div>
+                <button className='btn btn-success m-2'>Edit</button>
+                <button className='btn btn-danger'>Delete</button>
+            </div>
+        ),
     }
-
-];
-const data = [
-    {
-        key: '1',
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-    },
-    {
-        key: '3',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-    },
-    {
-        key: '4',
-        name: 'Disabled User',
-        age: 99,
-        address: 'Sydney No. 1 Lake Park',
-    },
 ];
 
-
-
-const Emplyee = () => {
-    const { data: employees} = useGetEmplyeeQuery();
+const Employee = () => {
+    const { data: employees } = useGetEmplyeeQuery()
     console.log(employees);
 
-    const [selectionType, setSelectionType] = useState('checkbox');
+    // Mapping API data to match Ant Design's expected format
+    const dataSource = employees?.data?.data?.map((employee) => ({
+        key: employee.id,
+        name: employee.name,
+        email: employee.email,
+        phone: employee.phone,
+        address: employee.address,
+        achievement: employee.achievements?.[0]?.name || 'N/A', // If you have this field in the data
+    }));
+
     return (
         <div>
             <Container className='mt-5'>
                 <Row>
-                    <Col md={9} lg={9} sm={12} className="m-auto" >
+                    <Col md={9} lg={9} sm={12} className="m-auto">
                         <Table
                             columns={columns}
-                            dataSource={data}
+                            dataSource={dataSource}
                         />
                     </Col>
                 </Row>
             </Container>
         </div>
     );
-
 };
-export default Emplyee;
+
+export default Employee;
